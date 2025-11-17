a186327a7dea25c37df381ecc66afff3caabe4ad
MadiBrom <madibromfield215@gmail.com>
2025-11-17 00:19:44 -0600

    adding local host?

--- yarnia-project/src/API/index.js
+++ yarnia-project/src/API/index.js
@@ -1,4 +1,5 @@
-const API_URL =  import.meta.env.DATABASE_URL;
+const API_URL =  import.meta.env.VITE_API_URL;
+export { API_URL };
 
 export const clearLocalStorage = () => {
   localStorage.clear();
